0f5b99112936a6950ad46a09e2ff79d105b671ee
owner <dave@davewells.me>
2026-08-02 14:24:59 -0500

    feat: add config module with env var support + config template

---
+++ scam_detector_config.py
@@ -0,0 +1,4 @@
+import os
+MAXMIND_ACCOUNT_ID = os.getenv("MAXMIND_ACCOUNT_ID", "1387848")
+MAXMIND_LICENSE_KEY = os.getenv("MAXMIND_LICENSE_KEY", "<your_key_here>")
+GEOIP_DATABASE_PATH = "/home/owner/Documents/scam-detector/data/Geolite2-Country.mmdb"
